66054fc818636b8ed38332da709e8458e81f97ac
pieleba <45750386+pieleba@users.noreply.github.com>
2019-03-30 10:19:50 +0800

    Delete 1.sql

--- 1.sql
+++
@@ -1,8 +0,0 @@
-select s.name,s.sex,a.c1,a.c2 froms
-(select sn,
-max(if(cn='01',score,0)) as c1,
-max(if(cn='02',score,0)) as c2
-from sc
-group by sn) as a
-join student s on (a.sn=s.sn)
-where a.c1<a.c2;
